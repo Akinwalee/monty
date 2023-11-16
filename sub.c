de95b6b2f53a7b13568644a3e397014e1580652f
Akinwalee <obatulafuad@gmail.com>
2023-11-16 18:29:56 +0000

    Updated files

---
+++ sub.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * sub - Subtracts the top item on the stack from the second top item.
+ *
+ * @top: Pointer to the top of the stack
+ *
+ * Return: void.
+ */
+
+void sub(stack_t **top)
+{
+	stack_t *current = *top;
+
+	current->prev->n -= current->n;
+	*top = current->prev;
+	free(current);
+}
