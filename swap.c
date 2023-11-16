c5f1617e227d6a6e0d1b964048732ffafcdeda5d
Akinwalee <obatulafuad@gmail.com>
2023-11-16 17:56:21 +0000

    Implemented swap function

---
+++ swap.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two items of the stack.
+ * 
+ * @top: Pointer to the top of the stack
+ *
+ * Return: void.
+ */
+
+void swap(stack_t **top)
+{
+	stack_t *current = *top;
+	stack_t *second = (*top)->prev;
+
+	current->prev = second->prev;
+	second->next = current->next;
+	current->next = second;
+	second->prev = current;
+
+	/**Updating the adjacent nodes**/
+	if (current->prev)
+		current->prev->next = current;
+	if (second->next)
+		second->next->prev = second;
+
+	*top = second;
+}
